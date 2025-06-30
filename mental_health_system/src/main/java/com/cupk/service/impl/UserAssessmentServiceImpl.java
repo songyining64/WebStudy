1f6530fb4ea31fb638d18a54639b031705618d7b
JiYe <2362707979@qq.com>
2025-06-30 11:26:23 +0800

    心理问卷接口实现

--- mental_health_system/src/main/java/com/cupk/service/impl/UserAssessmentServiceImpl.java
+++ mental_health_system/src/main/java/com/cupk/service/impl/UserAssessmentServiceImpl.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.ArrayList;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 @Service
 public class UserAssessmentServiceImpl extends ServiceImpl<UserAssessmentMapper, UserAssessment> implements UserAssessmentService {
